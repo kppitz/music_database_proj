e3eac79e762c0ba9e177381f991cb212e50abdd6
Katie Pitz <32210874+kppitz@users.noreply.github.com>
2018-04-26 12:54:03 -0500

    added table constraints

--- Tables.sql
+++ Tables.sql
@@ -19,66 +19,66 @@ CREATE Table Artist(
 );
 
 CREATE Table Album(
-	Album_name VARCHAR(30),
+	Album_name VARCHAR(30) NOT NULL,
 	Genre VARCHAR(30),
-	Artist_name VARCHAR(30),
+	Artist_name VARCHAR(30) NOT NULL,
 	PRIMARY KEY(Album_name, Artist_name),
 	FOREIGN KEY (Artist_name) REFERENCES Artist(Artist_name)
 );
 
 CREATE Table Song(
-	Song_name VARCHAR(30),
-	Song_length INT,
+	Song_name VARCHAR(30) NOT NULL,
+	Song_length INT NOT NULL,
 	Album_name VARCHAR(30),
-	Artist_name VARCHAR(30),
+	Artist_name VARCHAR(30) NOT NULL,
 	PRIMARY KEY(Song_name, Artist_name),
 	FOREIGN KEY (Album_name) REFERENCES Album(Album_name),
 	FOREIGN KEY (Artist_name) REFERENCES Artist(Artist_name)
 );
 
 CREATE Table User_Account(
-	User_name VARCHAR(30) PRIMARY KEY,
-	Join_date DATE,
-	Email VARCHAR(30),
+	User_name VARCHAR(30) NOT NULL PRIMARY KEY,
+	Join_date DATE NOT NULL,
+	Email VARCHAR(30) NOT NULL,
 	First_name VARCHAR(30),
 	Last_name VARCHAR(30)
 );
 
 CREATE Table Free_User(
-	User_name VARCHAR(30) PRIMARY KEY,
-	Free_time_left INT,
+	User_name VARCHAR(30) NOT NULL PRIMARY KEY,
+	Free_time_left INT NOT NULL CHECK( >0),
 	FOREIGN KEY (User_name) REFERENCES User_Account(User_name)
 );
 
 CREATE Table Paying_User(
-	Subscription_start_date DATE,
-	Payment_type VARCHAR(30),
-	Renewal_date DATE,
-	User_name VARCHAR(30) PRIMARY KEY,
+	Subscription_start_date DATE NOT NULL,
+	Payment_type VARCHAR(30) NOT NULL,
+	Renewal_date DATE NOT NULL,
+	User_name VARCHAR(30) NOT NULL PRIMARY KEY,
 	FOREIGN KEY (User_name) REFERENCES User_Account(User_name)
 );
 CREATE Table Playlist(
-	Playlist_name VARCHAR(30),
+	Playlist_name VARCHAR(30) NOT NULL,
 	Num_songs INT,
 	Playlist_length INT,
-	User_name VARCHAR(30),
+	User_name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (Playlist_name, User_name),
 	FOREIGN KEY (User_name) REFERENCES User_Account(User_name)
 );
 
 CREATE Table Personal_Playlist(
-	Date_created DATE,
-	Playlist_name VARCHAR(30),
-	User_name VARCHAR(30),
+	Date_created DATE NOT NULL,
+	Playlist_name VARCHAR(30) NOT NULL,
+	User_name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (Playlist_name, User_name),
 	FOREIGN KEY (User_name) REFERENCES User_Account(User_name),
 	FOREIGN KEY (Playlist_name) REFERENCES Playlist(Playlist_name)
 );
 
 CREATE Table Other_User_Playlist(
-	Original_owner VARCHAR(30),
-	Playlist_name VARCHAR(30),
-	User_name VARCHAR(30),
+	Original_owner VARCHAR(30) NOT NULL,
+	Playlist_name VARCHAR(30) NOT NULL,
+	User_name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (Original_owner, Playlist_name, User_name),
 	FOREIGN KEY (User_name) REFERENCES User_Account(User_name),
 	FOREIGN KEY (Playlist_name) REFERENCES Playlist(Playlist_name)
@@ -93,9 +93,9 @@ CREATE Table Produce(
 );
 
 CREATE Table Include(
-	Song_name VARCHAR(30),
-	Artist_name VARCHAR(30),
-	Album_name VARCHAR(30),
+	Song_name VARCHAR(30) NOT NULL,
+	Artist_name VARCHAR(30) NOT NULL,
+	Album_name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (Song_name, Artist_name, Album_name),
 	FOREIGN KEY (Song_name) REFERENCES Song(Song_name),
 	FOREIGN KEY (Artist_name) REFERENCES Artist(Artist_name),
@@ -103,11 +103,11 @@ CREATE Table Include(
 );
 
 CREATE Table Add_Song(
-	User_name VARCHAR(30),
-	Playlist_name VARCHAR(30),
-	Song_name VARCHAR(30),
-	Artist_name VARCHAR(30),
-	Album_name VARCHAR(30),
+	User_name VARCHAR(30) NOT NULL,
+	Playlist_name VARCHAR(30) NOT NULL,
+	Song_name VARCHAR(30) NOT NULL,
+	Artist_name VARCHAR(30) NOT NULL,
+	Album_name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (User_name, Playlist_name, Song_name, Artist_name, Album_name),
 	FOREIGN KEY (User_name) REFERENCES User_Account(User_name),
 	FOREIGN KEY (Playlist_name) REFERENCES Playlist(Playlist_name),
@@ -117,13 +117,13 @@ CREATE Table Add_Song(
 );
 
 CREATE Table Create_Playlist(
-	User_name VARCHAR(30),
-	Playlist_name VARCHAR(30),
+	User_name VARCHAR(30) NOT NULL,
+	Playlist_name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (User_name, Playlist_name),
 	FOREIGN KEY (User_name) REFERENCES User_Account(User_name),
 	FOREIGN KEY (Playlist_name) REFERENCES Playlist(Playlist_name)
 );
-	
+
 DROP TABLE Create_Playlist;
 DROP TABLE Add_Song;
 DROP TABLE Include;
